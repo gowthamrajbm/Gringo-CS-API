5d042108344437fe7c9b950aec38bc17f3657971
gowthamrajbm <gowthamrajbm@gmail.com>
2019-05-21 00:01:51 +0530

    Update production.js

--- server/config/production.js
+++ server/config/production.js
@@ -1,3 +1,23 @@
 module.exports = {
-  logging: false
+  logging: true,
+  seed: false,
+  db: {
+    url: 'mongodb://localhost/gringo'
+  },
+	media: {
+		upload: {
+			to: 's3', // or 'local-fs'
+			s3: {
+				bucket: '',
+				secretAccessKey: '',
+				accessKeyId: '',
+				region: 'ap-south-1',
+				accessUrl: ''
+			},
+			local: {
+				path: '/home/abhi/bucket/gringo/uploads',
+				accessPath: '/uploads'
+			}
+		}
+	}
 }
